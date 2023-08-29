4dc78d52e273e93a96e54145a89409ee1f6513e2
Aluno Lab 135 <lab135@lab.senai>
2023-08-29 19:08:56 -0300

    sequencia cresente

---
+++ crescente.py
@@ -0,0 +1,6 @@
+num1 = 0
+# Versão 1 Funcionalidade esperada: seu programa deverá mostrar a sequência 0,25,50,75,100,125,150,175,200 (utilize loops)
+while (num1 <= 200):
+    print(num1)
+    num1 = num1 + 25
+print("")
